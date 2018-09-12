671d51bac66feba3ad7db03dba58f5a7ccd8f52e
guilhem <guilhem.marchand@gmail.com>
2018-09-12 10:46:52 +0100

    Initial commit

---
+++ DA-ITSI-TELEGRAF-OS/README
@@ -0,0 +1,4 @@
+# ITSI Module for Influxdata Telegraf Operating System metrics
+
+## On-going work - please come back very soon !!!
+
--- appinspect.sh
+++ appinspect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-splunk-appinspect inspect `ls DA-ITSI-METRICATOR-NMON_*.tgz | head -1` --mode precert --included-tags splunk_appinspect
+splunk-appinspect inspect `ls DA-ITSI-TELEGRAF-OS_*.tgz | head -1` --mode precert --included-tags splunk_appinspect
